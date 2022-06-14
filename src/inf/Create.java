d17e476b858a2ec96b8ee8dc0f57233cf568a81d
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-15 00:01:56 +0900

    setText 추가

--- src/inf/Create.java
+++ src/inf/Create.java
@@ -1,6 +1,12 @@
 package inf;
 
 public interface Create {
+    String buttonNull = "버튼이 Null 입니다.";
+    String checkBoxNull = "체크박스가 Null 입니다.";
+    String frameNull = "프레임이 Null 입니다.";
+    String labelNull = "라벨이 Null 입니다.";
+
+    void setText(String text);
     void create(String text);
     void create();
 }
