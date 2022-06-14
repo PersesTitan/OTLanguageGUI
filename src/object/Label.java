6f388e17975fc5cc8fd502f76c16d176394387f9
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-15 00:01:54 +0900

    오타 수정

--- src/object/Label.java
+++ src/object/Label.java
@@ -7,12 +7,12 @@ import javax.swing.*;
 public class Label implements Create {
 
     @Override
-    public void crate(String text) {
+    public void create(String text) {
         JLabel label = new JLabel(text);
     }
 
     @Override
-    public void crate() {
+    public void create() {
         JLabel label = new JLabel();
     }
 }
