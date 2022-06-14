a937af91b7e174bb8ee5a3d0ed3b6566ac453581
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-15 00:03:01 +0900

    checkbox 생성

--- src/object/CheckBox.java
+++ src/object/CheckBox.java
@@ -1,4 +1,26 @@
 package object;
 
-public class CheckBox {
+import inf.Create;
+
+import javax.swing.*;
+
+public class CheckBox implements Create {
+
+    private JCheckBox checkBox;
+
+    @Override
+    public void setText(String text) {
+        if (checkBox == null) throw new NullPointerException(checkBoxNull);
+        checkBox.setText(text);
+    }
+
+    @Override
+    public void create(String text) {
+        this.checkBox = new JCheckBox(text);
+    }
+
+    @Override
+    public void create() {
+        this.checkBox = new JCheckBox();
+    }
 }
