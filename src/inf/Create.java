234ea0f146650d5113a0dcf0eadeae35e2c0d542
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-29 11:45:01 +0900

    check 추가

--- src/inf/Create.java
+++ src/inf/Create.java
@@ -5,8 +5,11 @@ public interface Create {
     String checkBoxNull = "체크박스가 Null 입니다.";
     String frameNull = "프레임이 Null 입니다.";
     String labelNull = "라벨이 Null 입니다.";
+    String textFieldNull = "텍스트필드가 Null 입니다.";
+    String passwordFieldNull = "페스워드필드가 Null 입니다.";
 
     void setText(String text);
     void create(String text);
     void create();
+    boolean check(String line);
 }
