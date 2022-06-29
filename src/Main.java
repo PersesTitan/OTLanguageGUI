6757bf5fd09ca51397c3a9e7083f67b1189700a2
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-29 11:45:25 +0900

    메인 실행 코드 추가

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import java.io.File;
+import java.util.Locale;
+
 public class Main {
 
-    public static void main(String[] args) {
+    private Main(String[] args) throws Exception {
+        if (args.length < 1) throw new Exception("파일이 존재하지 않습니다.");
 
+        String path = args[0];
+        if (new File(path).canRead()) throw new Exception("파일을 읽을 수 없습니다.");
+        boolean bool = path.toLowerCase(Locale.ROOT).endsWith(".otl");
+        bool = bool || path.toLowerCase(Locale.ROOT).endsWith(".otlg");
+        if (!bool) throw new Exception("읽을 수 없는 확장자 입니다");
+    }
+
+    public static void main(String[] args) {
+        try {
+            new Main(args);
+        } catch (Exception e) {
+            String error = e.getMessage();
+
+        }
     }
 }
