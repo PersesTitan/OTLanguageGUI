5ed703c13a778a0851186b7b6eb59c695cef2940
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-29 11:56:38 +0900

    Pattern, check, create, setText 추가

--- src/object/TextField.java
+++ src/object/TextField.java
@@ -1,8 +1,33 @@
 package object;
 
+import inf.Create;
+
 import javax.swing.*;
+import java.util.regex.Pattern;
 
-public class TextField {
-
+public class TextField implements Create {
+    private final static String PATTERN = "(^|\\s+|\\n)ㄱㅌㄱ($|\\s+)";
+    private final static Pattern pattern = Pattern.compile(PATTERN);
     private JTextField textField;
+
+    @Override
+    public void setText(String text) {
+        if (textField == null) throw new NullPointerException(textFieldNull);
+        textField.setText(text);
+    }
+
+    @Override
+    public void create(String text) {
+        textField = new JTextField(text);
+    }
+
+    @Override
+    public void create() {
+        textField = new JTextField();
+    }
+
+    @Override
+    public boolean check(String line) {
+        return pattern.matcher(line).find();
+    }
 }
