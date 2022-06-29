cc332e2fb2d72fa3594aec023eec3787530239cb
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-30 01:42:33 +0900

    Setting 클래스 추가

---
+++ src/item/Setting.java
@@ -0,0 +1,11 @@
+package item;
+
+import inf.Objects;
+
+public class Setting implements Objects {
+
+    public void start(String line) {
+        if (button.check(line)) button.create();
+
+    }
+}
