2bb072321275bfe6774aab9a4592fe0076bd1d4c
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-29 11:45:45 +0900

    오프젝트 생성 코드 추가

---
+++ src/inf/Objects.java
@@ -0,0 +1,12 @@
+package inf;
+
+import object.*;
+
+public interface Objects {
+    Button button = new Button();
+    CheckBox checkBox = new CheckBox();
+    Frame frame = new Frame();
+    Label label = new Label();
+    PasswordField passwordField = new PasswordField();
+    TextField textField = new TextField();
+}
